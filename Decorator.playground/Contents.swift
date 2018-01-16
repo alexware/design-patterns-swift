e1079465b7ead2b1ff064f7769acdaa611753b05
Oleh Zayats <ozayats@gmail.com>
2018-01-16 09:47:31 +0200

    corrections

--- Decorator.playground/Contents.swift
+++ Decorator.playground/Contents.swift
@@ -21,82 +21,109 @@ protocol ProductProtocol {
 }
 
 class Product: ProductProtocol {
+    
     let name: String
-    let description: String
     let category: ProductCategory
-    let price: Double
+    
+    var description: String {
+        get { return _description }
+    }
+    
+    var price: Double {
+        get { return _price }
+    }
+    
+    private let _description: String
+    private let _price: Double
     
     init(name: String, description: String, category: ProductCategory, price: Double) {
         self.name = name
-        self.description = description
         self.category = category
-        self.price = price
+        self._description = description
+        self._price = price
     }
 }
 
 class ProductPriceDecorator: Product {
-    private let baseProduct: Product
-
+    let baseProduct: Product
+    
     init(_ baseProduct: Product) {
         self.baseProduct = baseProduct
-        super.init(name: baseProduct.name, description: baseProduct.description, baseProduct.category: String, price: baseProduct.price)
+        super.init(name: baseProduct.name, description: baseProduct.description, category: baseProduct.category, price: baseProduct.price)
     }
 }
 
-class ProductBlackFridayPriceDecorator: ProductPriceDecorator {
+class ProductBlackFridayDecorator: ProductPriceDecorator {
+    override var description: String {
+        return baseProduct.description + "This is Black Friday! Discount: \(baseProduct.price - price) $"
+    }
+    
     override var price: Double {
-        var price = wrappedBaseProduct.price
-        
+        var price = baseProduct.price
+
         switch category {
-            
+
         case .techMobile:
             price = price * 0.95
-            
+
         case .techAccessories:
             price = price * 0.65
-            
+
         case .techComputerAndOffice, .techConsumerElectronics, .techHomeImprovement:
             price = price * 0.95
-            
+
         case .clothesWomen:
             price = price * 0.60
-            
+
         case .clothesMen:
             price = price * 0.90
-            
+
         case .clothesKids:
             price = price * 0.60
-            
+
         case .toys:
             price = price * 0.80
         }
-        return price;
+        return price
     }
 }
 
-class ProductCyberMondayPriceDecorator: ProductPriceDecorator {
+class ProductCyberMondayDecorator: ProductPriceDecorator {
+    override var description: String {
+        return baseProduct.description + "Discounts for ALL! Happy Cyber Monday!"
+    }
+    
     override var price: Double {
-        var price = wrappedBaseProduct.price
-        
+        var price = baseProduct.price
+
         switch category {
-            
+
         case .techMobile:
             price = price * 0.80
-            
+
         case .techAccessories:
             price = price * 0.50
-            
+
         case .techComputerAndOffice, .techConsumerElectronics:
             price = price * 0.75
-            
+
         case .techHomeImprovement:
             price = price * 0.85
-            
+
         default:
             break
         }
-        return price;
+        return price
     }
 }
 
+let iPhoneX = Product(name: "iPhoneX", description: "Has animated 'shit' emoji!", category: .techMobile, price: 1800)
+
+let iPhoneXBlackFridayDecorator = ProductBlackFridayDecorator(iPhoneX)
+let iPhoneXCyberMondayDecorator = ProductCyberMondayDecorator(iPhoneX)
+
+let blPrice = iPhoneXBlackFridayDecorator.price
+let blDescription = iPhoneXBlackFridayDecorator.description
+let cmPrice = iPhoneXCyberMondayDecorator.price
+let cmDescription = iPhoneXCyberMondayDecorator.description
 
