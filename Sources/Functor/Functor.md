e684e0224c09fcb373ec95462f35675f11142480
Zayats Oleh <oleh.zayats@globallogic.com>
2018-01-28 13:00:51 +0200

    functor playground additions

--- Sources/Functor/Functor.md
+++ Sources/Functor/Functor.md
@@ -3,18 +3,19 @@
 
 ### Description </br>
 Functor is a structure preserving map between categories. </br>
-
+In Swift a Functor is some type 'A' it could be a Collection like Array, Dictionary etc. or Enumeration or even a Promise which contains value of type 'B' and can transform it via map function: </br>
+A<B> -> A<C>, map(B -> C) </br>
 
 ### iOS/MacOS adaptation </br>
 Any type that implements 'map' in Swift will be a functor. </br>
 
-### Structure/UML*
+### Structure/UML
 --
 
-### Rules of thumb*
+### Rules of thumb
 --
 
-### Why and where you should use the pattern*
+### Why and where you should use the pattern
 
 ### Pros </br>
 
--- Sources/Functor/Functor.playground/Contents.swift
+++ Sources/Functor/Functor.playground/Contents.swift
@@ -1,11 +1,17 @@
 
 import Foundation
 
+/* 1. */
+
+/* Container (enumaration) */
+
+// Note: 'Result' is similar to Optional/Either, but returns Error instead, so that you know why a request failed
 enum Result<T> {
     case success(T)
     case failure(Error)
 }
 
+/* map implementation */
 extension Result {
     func map<U>(_ transform: (T) -> U) -> Result<U> {
         switch self {
@@ -16,3 +22,22 @@ extension Result {
         }
     }
 }
+
+/* 2 */
+
+/* functional style Functor */
+
+infix operator <^>
+
+func <^><T, U>(_ transform: (T) -> U, _ input: [T]) -> [U] {
+    return input.map(transform)
+}
+
+let toInteger: (String) -> Int = { return Int($0)! }
+let collectionOfStrings = ["10", "1000", "40"]
+
+let collectionOfIntegers = toInteger <^> collectionOfStrings
+
+// Caution (!): Exercise restraint when using custom operators with exotic symbols
+// they are hard to type, and therefore hard to use.
+
