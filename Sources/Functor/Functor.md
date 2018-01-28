6c00452ce57464d04d67233f29439a8202831f92
Zayats Oleh <oleh.zayats@globallogic.com>
2018-01-28 14:45:22 +0200

    functor pattern corrections

--- Sources/Functor/Functor.md
+++ Sources/Functor/Functor.md
@@ -3,14 +3,16 @@
 
 ### Description </br>
 Functor is a structure preserving map between categories. </br>
-In Swift a Functor is some type 'A' it could be a Collection like Array, Dictionary etc. or Enumeration or even a Promise which contains value of type 'B' and can transform it via map function: </br>
-A<B> -> A<C>, map(B -> C) </br>
+In Swift a Functor is some type 'A' it could be a Collection like Array, Dictionary etc. or Enumeration or even a Promise which contains value of type 'B' and can transform it via map function. </br>
+
 
 ### iOS/MacOS adaptation </br>
 Any type that implements 'map' in Swift will be a functor. </br>
 
 ### Structure/UML
---
+A<B> -> A<C>, map(B -> C) </br>
+
+fmap :: (a -> b) -> fa -> fb
 
 ### Rules of thumb
 --
@@ -22,4 +24,4 @@ Any type that implements 'map' in Swift will be a functor. </br>
 ### Cons </br>
 
 ### More
---
+http://www.mokacoding.com/blog/functor-applicative-monads-in-pictures/
--- Sources/Functor/Functor.playground/Contents.swift
+++ Sources/Functor/Functor.playground/Contents.swift
@@ -1,17 +1,41 @@
 
 import Foundation
 
-/* 1. */
+/* 1. Native Swift types */
 
-/* Container (enumaration) */
+let integers = [2, 3, 4, 5]
+let multipliedResult = integers.map({ return $0 * 2 })
 
-// Note: 'Result' is similar to Optional/Either, but returns Error instead, so that you know why a request failed
+// That's it, Array (any type that implements a 'map' function) is a Functor.
+
+// But lets make a pure functional Functor
+// Haskell is using <$> operator, but that would not compile in Swift...
+
+infix operator <^>
+
+func <^><T, U>(_ transform: (T) -> U, _ input: [T]) -> [U] {
+    return input.map(transform)
+}
+
+// Caution (!): Exercise restraint when using custom operators with exotic symbols
+// they are hard to type, and therefore hard to use.
+
+let strings = ["1", "2", "3", "5", "10"]
+let toInt: (String) -> Int = { return Int($0)! }
+
+let integersResult = toInt <^> strings
+
+/* 2. Custom types */
+
+// Custom type 'Result' is similar to Optional/Either, but returns Error instead,
+// so that you know why a request failed
+
 enum Result<T> {
     case success(T)
     case failure(Error)
 }
 
-/* map implementation */
+/* map implementation for Result type */
 extension Result {
     func map<U>(_ transform: (T) -> U) -> Result<U> {
         switch self {
@@ -22,22 +46,3 @@ extension Result {
         }
     }
 }
-
-/* 2 */
-
-/* functional style Functor */
-
-infix operator <^>
-
-func <^><T, U>(_ transform: (T) -> U, _ input: [T]) -> [U] {
-    return input.map(transform)
-}
-
-let toInteger: (String) -> Int = { return Int($0)! }
-let collectionOfStrings = ["10", "1000", "40"]
-
-let collectionOfIntegers = toInteger <^> collectionOfStrings
-
-// Caution (!): Exercise restraint when using custom operators with exotic symbols
-// they are hard to type, and therefore hard to use.
-
